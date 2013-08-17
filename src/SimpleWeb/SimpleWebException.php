4ababc8a207c25cde9ef9b608cdd7447c7b648ff
robertlabrie <robert.labrie@gmail.com>
2013-08-17 11:45:22 -0400

    add namespace and overrides

--- src/SimpleWeb/SimpleWebException.php
+++ src/SimpleWeb/SimpleWebException.php
@@ -1,2 +1,13 @@
 <?php
-class SimpleWebException extends Exception { }
+namespace SimpleWeb;
+class SimpleWebException extends \Exception
+{ 
+	public $httpCode;
+	public $shortMessage;
+	public function __construct($message, $code = 0, Exception $previous = null, $httpCode = 0, $shortMessage = null)
+	{
+		$this->httpCode = $httpCode;
+		$this->shortMessage = $shortMessage;
+		parent::__construct($message, $code, $previous);
+	}
+}
